6cb33d7309cb2418d8ac43e6cec3cc16e7c5c7f9
Ivan Borshchov <ivan@devforth.io>
2022-02-03 14:34:56 +0200

    gptneo fixes

--- web.py
+++ web.py
@@ -1,4 +1,8 @@
+from pandas import datetime
 import cherrypy
+import datetime
+print('Before model import')
+before = datetime.datetime.now()
 import model
 
 class MyWebService(object):
@@ -11,7 +15,10 @@ class MyWebService(object):
         output = {"output": model.eval(data)}
         return output
 
+print('Inititalizing, model imported within', datetime.datetime.now() - before)
+
 if __name__ == '__main__':
     config = {'server.socket_host': '0.0.0.0'}
+    print('Binding to 0.0.0.0')
     cherrypy.config.update(config)
     cherrypy.quickstart(MyWebService())
